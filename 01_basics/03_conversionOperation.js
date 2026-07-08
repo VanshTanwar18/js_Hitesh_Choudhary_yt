1b8f1bc63cca45abc372cd2a8d1c16cf5be6d2dd
VanshTanwar18 <vanshtanwar18@gmail.com>
2026-07-08 02:50:38 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,8 +15,8 @@ let isLoggedIn = "hitesh";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
-console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
 
 // 1 => true; 0 => false;
 // "" => false;
@@ -25,5 +25,36 @@ console.log(typeof(booleanIsLoggedIn));
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* Operations *****************
+
+let value = 3;
+let negValue = -value;
+// console.log(value);
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " hitesh"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
