3bba58150662ec7b4731ec6746ba64b712f497d8
VanshTanwar18 <vanshtanwar18@gmail.com>
2026-07-10 10:48:23 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,23 @@
+// for 
+
+for (let i = 0;i<10;i++) {
+    const ele = i;
+    console.log(ele);
+}
+
+let myArray = ["flash","batman","superman"];
+
+for (let i = 0;i<myArray.length;i++) {
+    const element = myArray[i];
+    console.log(element);
+}
+
+// break and continue;
+
+for (let i = 0;i<20;i++) {
+    if (i == 5) {
+        console.log("5 detected");
+        break;
+    }
+}
+
