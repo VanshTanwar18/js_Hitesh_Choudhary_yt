60a19595a3e51649d073971a183a2f5538d0d322
VanshTanwar18 <vanshtanwar18@gmail.com>
2026-07-14 08:54:54 +0000

    events

---
+++ 07_projects/05_unlimitedColors/chaiaurcode.js
@@ -0,0 +1,32 @@
+//generate a random color;
+
+const randomColor = function() {
+    const hex  = "0123456789ABCDEF";
+    let color = '#';
+
+    for (let i = 0;i<6;i++) {
+        color += hex[Math.floor(Math.random()*16)]
+
+    }
+    return color;
+};
+
+let intervalId = null;
+const startChangingColor = function() {
+    
+    if (!intervalId) intervalId = setInterval(changeByColor,1000);
+
+    function changeByColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+
+const stopChangingColor = function() {
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
+
+document.querySelector('#start').addEventListener(('click'),startChangingColor);
+
+document.querySelector('#stop').addEventListener(('click'),stopChangingColor);
